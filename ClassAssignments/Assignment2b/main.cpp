1f656b33f833f9b807714069d30aa7bd97c5176a
Vegas <64249089+ignVegas@users.noreply.github.com>
2024-10-08 14:09:16 -0500

    Update main.cpp

--- ClassAssignments/Assignment2b/main.cpp
+++ ClassAssignments/Assignment2b/main.cpp
@@ -1,1 +1,31 @@
 
+#include <iostream>
+using namespace std;
+
+class One {
+private:
+	int x, y;
+public:
+
+	void set(int valy, int valx = 0) {
+		x = valx;
+		y = valy;
+	}
+
+	int getX() const {
+		return x;
+	}
+
+	int getY() const {
+		return y;
+	}
+};
+
+int main() {
+	One o;
+	o.set(20);
+	cout << "X: " << o.getX() << endl;
+	cout << "Y: " << o.getY() << endl;
+
+	return 0;
+}
